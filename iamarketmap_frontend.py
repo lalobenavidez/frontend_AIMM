14186452404a190fb105ea1bbe823f2cb6ba10fc
Eduardo Benavidez <lalobenavidez@gmail.com>
2025-06-18 18:33:19 -0400

    quitar debugs

--- iamarketmap_frontend.py
+++ iamarketmap_frontend.py
@@ -454,11 +454,7 @@ if bloques:
                     target_y = 0.1
                     dist_target = last - target
                     dist_stop = stop - last
-                    pos_stop = last_y + (dist_stop / dist_target) * (last_y - target_y) if dist_target != 0 else 0.9
-                    pos_stop = min(0.9, max(pos_stop, 0.51))
-                    # 🔁 Nueva regla: si queda muy cerca de 0.5, lo forzamos más arriba
-                    if pos_stop < 0.55:
-                        pos_stop = 0.65
+                    pos_stop = 0.8
 
 
                 y_vals = [pos_stop, last_y, target_y]
