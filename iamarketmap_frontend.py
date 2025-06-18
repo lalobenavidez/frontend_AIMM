c1da5455f77e2dc1760d565967f90a289bbcd26f
Eduardo Benavidez <lalobenavidez@gmail.com>
2025-06-18 17:45:34 -0400

    quitar debugs

--- iamarketmap_frontend.py
+++ iamarketmap_frontend.py
@@ -535,6 +535,9 @@ if bloques:
 #    unsafe_allow_html=True,
 #)
 
+st.markdown("<div style='margin-top: 30px;'></div>", unsafe_allow_html=True)
+
+
 # --- Título y datos del ticker seleccionado ---
 st.markdown(f"""
 <div style="background-color:#1e2533; padding: 28px 32px 32px 32px; border-radius: 18px; margin-bottom: 20px;">
